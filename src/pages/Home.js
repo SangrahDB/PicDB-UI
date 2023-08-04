6f5881f72fdc4a06cc25dfab545dd79dec02ca97
heimanpictures <heimanpicturesofficial@gmail.com>
2023-08-04 20:59:46 +0530

    Checking update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -38,7 +38,7 @@ function Home() {
         },
       };
       
-      axios.post('https://picdb.onrender.com/api/v1/upload', formData, config)
+      axios.post('http://picdb.onrender.com/api/v1/upload', formData, config)
         .then((response) => {
           console.log(response.data['success']);
           if (response.data['success'] === "true") {
